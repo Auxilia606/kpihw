28dfac11d6c49618806513b62f2e2286014498a2
youngtaek_hong <youngtaek_hong@dweax.com>
2024-08-09 14:14:35 +0900

    Refactor: CustomFont constant 분리

--- src/shared/components/CustomText/styles.ts
+++ src/shared/components/CustomText/styles.ts
@@ -1,15 +1,17 @@
 import {StyleSheet} from 'react-native';
 
+const LETTER_SPACING_CONST = -0.05;
+
 const styles = StyleSheet.create({
   // font weight
   normal: {fontWeight: '400'},
-  medium: {fontWeight: '600'},
+  medium: {fontWeight: '500'},
   bold: {fontWeight: '700'},
 
   // font size
-  sm: {fontSize: 8, lineHeight: 20, letterSpacing: -8 * 0.05},
-  md: {fontSize: 12, lineHeight: 23, letterSpacing: -12 * 0.05},
-  lg: {fontSize: 20, lineHeight: 23, letterSpacing: -20 * 0.05},
+  sm: {fontSize: 8, lineHeight: 20, letterSpacing: 8 * LETTER_SPACING_CONST},
+  md: {fontSize: 12, lineHeight: 23, letterSpacing: 12 * LETTER_SPACING_CONST},
+  lg: {fontSize: 20, lineHeight: 23, letterSpacing: 20 * LETTER_SPACING_CONST},
 });
 
 export default styles;

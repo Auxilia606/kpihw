4a3e59e4cd804cab30adc24a7d73931b533a08f9
youngtaek_hong <youngtaek_hong@dweax.com>
2024-08-12 14:17:48 +0900

    Chore: entity 형식 템플릿 추가

---
+++ src/entities/_template/entity.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {TemplateProps} from './types';
+
+const Template = (props: TemplateProps) => {
+  console.log(props);
+  return <></>;
+};
+
+export default Template;

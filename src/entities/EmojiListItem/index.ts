42386f5db29cb21d31d5ec423ddacb156a69d3b7
youngtaek_hong <youngtaek_hong@dweax.com>
2024-08-12 13:36:27 +0900

    Feat: 이모지 리스트 아이템 추가

---
+++ src/entities/EmojiListItem/index.ts
@@ -0,0 +1,5 @@
+import EmojiListItem from './entity';
+
+export * from './types';
+
+export default EmojiListItem;
---
+++ src/entities/EmojiListItem/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  emojiContainer: {
+    width: 80,
+    height: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emojiIcon: {width: 48, height: 48},
+});
+
+export default styles;

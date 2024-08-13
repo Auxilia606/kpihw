345e1eb7b41a8bb13f215c868f40f63256e1b497
youngtaek_hong <youngtaek_hong@dweax.com>
2024-08-13 10:23:02 +0900

    Feat: CustomText xl 사이즈 추가

--- src/shared/components/CustomText/types.ts
+++ src/shared/components/CustomText/types.ts
@@ -4,7 +4,7 @@ import {StyleProp, TextStyle} from 'react-native';
 import styles from './styles';
 
 export type CustomTextProps = PropsWithChildren & {
-  fontSize?: Extract<keyof typeof styles, 'sm' | 'md' | 'lg'>;
+  fontSize?: Extract<keyof typeof styles, 'sm' | 'md' | 'lg' | 'xl'>;
   fontWeight?: Extract<keyof typeof styles, 'bold' | 'normal' | 'medium'>;
   style?: StyleProp<TextStyle>;
 } & Pick<TextStyle, 'color' | 'textAlign'>;
